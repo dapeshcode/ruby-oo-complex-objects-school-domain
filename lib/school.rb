b524cb78bb5d3ce6528aefdee18722c9a70a1f33
dapeshcode <ampeccia@gmail.com>
2021-02-02 17:00:52 -0600

    commit tree 8b6d2f6b6c2ac5dbbd7f775c03a76fdc2ccece53

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
-# code here!
+class School 
+  
+    def initialize(school_name)
+        @name = school_name
+        @roster = {}
+
+        
+    end
+    def name 
+        @name 
+    end 
+
+
+    def roster 
+        @roster
+    
+    end 
+
+    def add_student(student_name, grade)
+
+        if roster[grade]
+            roster[grade] << student_name
+        else 
+            roster[grade] = []
+            roster[grade] << student_name 
+        end 
+    end 
+
+    def grade(num)
+        roster[num]
+    end 
+
+    def sort 
+        sorted_roster = {}
+        roster.each do |grade, student_names|
+          sorted_roster[grade] = student_names.sort
+        end 
+        sorted_roster
+    end 
+end 
+
+
+
+
